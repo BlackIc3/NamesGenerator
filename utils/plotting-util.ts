1d623889a8e0495f8637971c33bbd429c2b664d2
Stefan Gruber <stefan@gruber-muc.de>
2021-04-19 18:10:01 +0200

    Plotting on hold

--- utils/plotting-util.ts
+++ utils/plotting-util.ts
@@ -1,26 +1,46 @@
 import { IAnalysisResult } from "../models/analysisResultModel";
-import { plot, Plot } from 'nodeplotlib';
+import { Layout, plot, Plot } from 'nodeplotlib';
 import { Color } from "plotly.js";
 import { Poi } from "../models/poi";
 
 export class Plotter {
 
-    public static plotResult(result:IAnalysisResult) {
+    /**
+     * Plots the given cluster data as a scatter plot. Calling this function  
+     * will open up a new browser window with the interactive plot
+     * @param cluster the cluster to plot
+     * @param title the title to display for the plot
+     */
+    public static plotCluster(cluster:Poi[][], title?:string) {
         const data: Plot[] = [
-            this.mapToTrace(result.clusteredPois[1], "test", "#1F85DE"),
-            this.mapToTrace(result.clusteredPois[2], "test", "#de581f"),
+            ...cluster.map((list, index) => this.mapToTrace(list, `Cluster #${index + 1}`, ''))
         ];
-        plot(data)
+        const layout: Layout = {
+            title: !!title ? title : "Cluster Data",
+            width: 1500,
+            height: 900,
+        }
+        plot(data, layout);
     }
 
-    private static mapToTrace(pois:Poi[], key:string, color:Color): Plot {
-        return {
+    /**
+     * Maps a list of POIs to a Plot-object
+     * @param pois the pois to map
+     * @param key the key to name the data
+     * @param color the color to use for this cluster
+     * @returns a Plot-object that can be plotted
+     */
+    private static mapToTrace(pois:Poi[], key:string, color?:Color): Plot {
+        const plot:Plot = {
             type: 'scattergeo',
             lon: pois.map((p) => p.long),
             lat: pois.map((p) => p.lat),
             mode: 'markers',
-            color: color,
-            text: key
-        } as Plot
+            name: key,
+        };
+
+        if (!!color) plot["marker.color"] = color;
+
+        return plot
     }
 }
