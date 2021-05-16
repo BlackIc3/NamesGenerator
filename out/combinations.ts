b6d699e533c4ba3cb2d457566bf0d470c5f6b4f2
Stefan Gruber <stefan@gruber-muc.de>
2021-05-16 15:35:48 +0200

    Add complete combinations.ts

--- out/combinations.ts
+++ out/combinations.ts
@@ -7,8 +7,18 @@ export const combinations: ICombination[] = [
 		total: 23433,
 		biggestCluster: 9137,
 		clusters: 109,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'spaßig', 'freudig', 'lustig', 'aufregend', 'spannend', 'großartig', 'schwungvoll', 'heiter'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Spielplatz'},
+			{adjectiveEnding: 'e', description: 'Spielstätte'},
+			{adjectiveEnding: 'er', description: 'Platz'},
+			{adjectiveEnding: 'es', description: 'Feld'},
+			{adjectiveEnding: 'es', description: 'Spielfeld'},
+			{adjectiveEnding: 'er', description: 'Kinderspielplatz'},
+			{adjectiveEnding: 'e', description: 'Spielfläche'},
+			{adjectiveEnding: 'er', description: 'Treffpunkt'},
+			{adjectiveEnding: 'er', description: 'Vergnügungspark'},
+		]
 	},
 	{
 		// 9 * 9
@@ -16,8 +26,18 @@ export const combinations: ICombination[] = [
 		total: 72861,
 		biggestCluster: 8544,
 		clusters: 86,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'versteckt', 'verborgen', 'alt', 'anmutig', 'beachtlich', 'geheim', 'dunkl', 'rätselhaft'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Jagdstand'},
+			{adjectiveEnding: 'e', description: 'Jagdkanzel'},
+			{adjectiveEnding: 'e', description: 'Wildkanzel'},
+			{adjectiveEnding: 'er', description: 'Jagdsitz'},
+			{adjectiveEnding: 'er', description: 'Hochsitz'},
+			{adjectiveEnding: 'er', description: 'Hochtsand'},
+			{adjectiveEnding: 'er', description: 'Stand'},
+			{adjectiveEnding: 'er', description: 'Ansitz'},
+			{adjectiveEnding: 'er', description: 'Anstand'},
+		]
 	},
 	{
 		// 9 * 9
@@ -25,8 +45,18 @@ export const combinations: ICombination[] = [
 		total: 83902,
 		biggestCluster: 8495,
 		clusters: 163,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'bequem', 'komfortabl', 'unbequem', 'schäbig', 'verkommen', 'reizend', 'schnucklig', 'schön'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Bank'},
+			{adjectiveEnding: 'e', description: 'Sitzbank'},
+			{adjectiveEnding: 'er', description: 'Rastplatz'},
+			{adjectiveEnding: 'e', description: 'Parkbank'},
+			{adjectiveEnding: 'er', description: 'Sitzplatz'},
+			{adjectiveEnding: 'e', description: 'Sitzgelegenheit'},
+			{adjectiveEnding: 'er', description: 'Platz'},
+			{adjectiveEnding: 'er', description: 'Treffpunkt'},
+			{adjectiveEnding: 'er', description: 'Sitz'},
+		]
 	},
 	{
 		// 9 * 9
@@ -34,8 +64,19 @@ export const combinations: ICombination[] = [
 		total: 12432,
 		biggestCluster: 8403,
 		clusters: 179,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'spannend', 'atemberaubend', 'erstaunlich', 'inspirierend', 'exklusiv', 'herausragend', 'überragend', 'berrauschend'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Aussichtspunkt'},
+			{adjectiveEnding: 'e', description: 'Aussicht'},
+			{adjectiveEnding: 'e', description: 'Sicht'},
+			{adjectiveEnding: 'er', description: 'Ausblick'},
+			{adjectiveEnding: 'er', description: 'Ausblickspunkt'},
+			{adjectiveEnding: 'e', description: 'Perspektive'},
+			{adjectiveEnding: 'er', description: 'Aussichtsort'},
+			{adjectiveEnding: 'e', description: 'Umgebung'},
+			{adjectiveEnding: 'es', description: 'Gebiet'}
+
+		]
 	},
 	{
 		// 9 * 9
@@ -43,8 +84,18 @@ export const combinations: ICombination[] = [
 		total: 11581,
 		biggestCluster: 8103,
 		clusters: 227,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'verwunschen', 'betörend', 'bezaubernd', 'faszinierend', 'bemerkenswert', 'außergewöhnlich', 'nass', 'idyllisch'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Fontäne'},
+			{adjectiveEnding: 'er', description: 'Brunnen'},
+			{adjectiveEnding: 'e', description: 'Wasserspiele'},
+			{adjectiveEnding: 'e', description: 'Wasserquelle'},
+			{adjectiveEnding: 'e', description: 'Wasserstelle'},
+			{adjectiveEnding: 'er', description: 'Wasserbrunnen'},
+			{adjectiveEnding: 'er', description: 'Springbrunnen'},
+			{adjectiveEnding: 'es', description: 'Wasser'},
+			{adjectiveEnding: 'es', description: 'Gewässer'},
+		]
 	},
 	{
 		// 8 * 8
@@ -52,8 +103,17 @@ export const combinations: ICombination[] = [
 		total: 11712,
 		biggestCluster: 7011,
 		clusters: 174,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'versteckt', 'sicher', 'schutzbietend', 'alt', 'verwunschen', 'dunkl', 'schäbig'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Hütte'},
+			{adjectiveEnding: 'er', description: 'Unterschlupf'},
+			{adjectiveEnding: 'er', description: 'Unterstand'},
+			{adjectiveEnding: 'e', description: 'Barracke'},
+			{adjectiveEnding: 'es', description: 'Häuschen'},
+			{adjectiveEnding: 'e', description: 'Kate'},
+			{adjectiveEnding: 'er', description: 'Schuppen'},
+			{adjectiveEnding: 'e', description: 'Scheune'},
+		]
 	},
 	{
 		// 8 * 8
@@ -61,8 +121,17 @@ export const combinations: ICombination[] = [
 		total: 108473,
 		biggestCluster: 6581,
 		clusters: 163,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'hölzern', 'morsch', 'idyllisch', 'abgelegen', 'malerisch', 'romantisch', 'gemütlich'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Holzbank'},
+			{adjectiveEnding: 'e', description: 'Bank'},
+			{adjectiveEnding: 'e', description: 'Parkbank'},
+			{adjectiveEnding: 'e', description: 'Sitzbank'},
+			{adjectiveEnding: 'e', description: 'Sitzgelegenheit'},
+			{adjectiveEnding: 'er', description: 'Sitz'},
+			{adjectiveEnding: 'er', description: 'Rastplatz'},
+			{adjectiveEnding: 'er', description: 'Platz'},
+		]
 	},
 	{
 		// 8 * 8
@@ -70,8 +139,17 @@ export const combinations: ICombination[] = [
 		total: 10989,
 		biggestCluster: 6308,
 		clusters: 199,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'beliebt', 'angenehm', 'betörend', 'festlich', 'magisch', 'verzaubernd', 'verwunschen'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Ort'},
+			{adjectiveEnding: 'er', description: 'Platz'},
+			{adjectiveEnding: 'er', description: 'Punkt'},
+			{adjectiveEnding: 'er', description: 'Fleck'},
+			{adjectiveEnding: 'es', description: 'Fleckchen'},
+			{adjectiveEnding: 'e', description: 'Ecke'},
+			{adjectiveEnding: 'er', description: 'Landstrich'},
+			{adjectiveEnding: 'es', description: 'Viertel'},
+		]
 	},
 	{
 		// 8 * 8
@@ -79,8 +157,17 @@ export const combinations: ICombination[] = [
 		total: 194207,
 		biggestCluster: 6068,
 		clusters: 104,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'schön', 'gut', 'kantig', 'schlecht', 'bekannt', 'unbekannt', 'geheim'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Bank'},
+			{adjectiveEnding: 'e', description: 'Parkbank'},
+			{adjectiveEnding: 'e', description: 'Sitzbank'},
+			{adjectiveEnding: 'e', description: 'Sitzgelegenheit'},
+			{adjectiveEnding: 'e', description: 'Sitzmöglichkeit'},
+			{adjectiveEnding: 'er', description: 'Sitzplatz'},
+			{adjectiveEnding: 'er', description: 'Platz'},
+			{adjectiveEnding: 'er', description: 'Pausenort'},
+		]
 	},
 	{
 		// 7 * 7
@@ -88,8 +175,16 @@ export const combinations: ICombination[] = [
 		total: 15320,
 		biggestCluster: 5179,
 		clusters: 204,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'klein', 'schattig', 'idyllisch', 'abgelegen', 'romantisch', 'malerisch'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Picknicktisch'},
+			{adjectiveEnding: 'er', description: 'Picknickplatz'},
+			{adjectiveEnding: 'er', description: 'Tisch'},
+			{adjectiveEnding: 'es', description: 'Örtchen'},
+			{adjectiveEnding: 'e', description: 'Sitzgruppe'},
+			{adjectiveEnding: 'e', description: 'Tafel'},
+			{adjectiveEnding: 'e', description: 'Picknicktafel'}
+		]
 	},
 	{
 		// 7 * 7
@@ -97,8 +192,16 @@ export const combinations: ICombination[] = [
 		total: 23629,
 		biggestCluster: 5176,
 		clusters: 95,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'klein', 'einladend', 'versteckt', 'verloren', 'heilsam', 'charmant'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Picknickplatz'},
+			{adjectiveEnding: 'es', description: 'Plätzchen'},
+			{adjectiveEnding: 'es', description: 'Örtchen'},
+			{adjectiveEnding: 'er', description: 'Platz'},
+			{adjectiveEnding: 'e', description: 'Picknickfläche'},
+			{adjectiveEnding: 'er', description: 'Verweilort'},
+			{adjectiveEnding: 'er', description: 'Fleck'},
+		]
 	},
 	{
 		// 5 * 5
@@ -106,8 +209,14 @@ export const combinations: ICombination[] = [
 		total: 12474,
 		biggestCluster: 2678,
 		clusters: 281,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'kalt', 'eisern', 'edl', 'rostig'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Metallbank'},
+			{adjectiveEnding: 'e', description: 'Stahlbank'},
+			{adjectiveEnding: 'e', description: 'Bank'},
+			{adjectiveEnding: 'e', description: 'Sitzbank'},
+			{adjectiveEnding: 'er', description: 'Sitzplatz'},
+		]
 	},
 	{
 		// 5 * 5
@@ -115,8 +224,15 @@ export const combinations: ICombination[] = [
 		total: 6388,
 		biggestCluster: 2494,
 		clusters: 92,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'bedeutsam', 'glänzend', 'ungeheur', 'schimmernd'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Schrein'},
+			{adjectiveEnding: 'es', description: 'Marterl'},
+			{adjectiveEnding: 'es', description: 'Kleindenkmal'},
+			{adjectiveEnding: 'er', description: 'Bildstock'},
+			{adjectiveEnding: 'es', description: 'Wegkreuz'},
+
+		]
 	},
 	{
 		// 5 * 5
@@ -124,8 +240,14 @@ export const combinations: ICombination[] = [
 		total: 8836,
 		biggestCluster: 2399,
 		clusters: 254,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'verflucht', 'verrucht', 'perfid', 'prächtig'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Sitzgruppe'},
+			{adjectiveEnding: 'e', description: 'Sitze'},
+			{adjectiveEnding: 'e', description: 'Bank'},
+			{adjectiveEnding: 'e', description: 'Sitzbank'},
+			{adjectiveEnding: 'e', description: 'Parkbank'},
+		]
 	},
 	{
 		// 5 * 5
@@ -133,8 +255,14 @@ export const combinations: ICombination[] = [
 		total: 4759,
 		biggestCluster: 2241,
 		clusters: 95,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'bedrohlich', 'verdächtig', 'göttlich', 'religiös'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Schrein'},
+			{adjectiveEnding: 'es', description: 'Marterl'},
+			{adjectiveEnding: 'es', description: 'Kleindenkmal'},
+			{adjectiveEnding: 'er', description: 'Bildstock'},
+			{adjectiveEnding: 'es', description: 'Wegkreuz'},
+		]
 	},
 	{
 		// 5 * 5
@@ -142,8 +270,14 @@ export const combinations: ICombination[] = [
 		total: 15519,
 		biggestCluster: 2047,
 		clusters: 283,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'unschön', 'hölzern', 'abgelegen', 'unscheinbar'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Holzbank'},
+			{adjectiveEnding: 'e', description: 'Bank'},
+			{adjectiveEnding: 'e', description: 'Sitzbank'},
+			{adjectiveEnding: 'e', description: 'Parkbank'},
+			{adjectiveEnding: 'er', description: 'Sitzplatz'},
+		]
 	},
 	{
 		// 4 * 4
@@ -151,8 +285,13 @@ export const combinations: ICombination[] = [
 		total: 11870,
 		biggestCluster: 1740,
 		clusters: 321,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'hart', 'unbeliebt', 'untauglich'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Bank'},
+			{adjectiveEnding: 'er', description: 'Pausenort'},
+			{adjectiveEnding: 'e', description: 'Sitzbank'},
+			{adjectiveEnding: 'e', description: 'Parkbank'},
+		]
 	},
 	{
 		// 4 * 4
@@ -160,8 +299,13 @@ export const combinations: ICombination[] = [
 		total: 8436,
 		biggestCluster: 1395,
 		clusters: 292,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'aufregend', 'belebt', 'lebendig'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Platz'},
+			{adjectiveEnding: 'er', description: 'Ort'},
+			{adjectiveEnding: 'e', description: 'Gegend'},
+			{adjectiveEnding:'er', description: 'Fleck'},
+		]
 	},
 	{
 		// 4 * 4
@@ -169,8 +313,13 @@ export const combinations: ICombination[] = [
 		total: 7294,
 		biggestCluster: 1316,
 		clusters: 180,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'morsch', 'karg', 'hilfreich'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Wildkanzel'},
+			{adjectiveEnding: 'e', description: 'Jagdkanzel'},
+			{adjectiveEnding: 'er', description: 'Unterstand'},
+			{adjectiveEnding: 'er', description: 'Jagdsitz'},
+		]
 	},
 	{
 		// 4 * 4
@@ -178,8 +327,13 @@ export const combinations: ICombination[] = [
 		total: 6314,
 		biggestCluster: 1267,
 		clusters: 225,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['','rettend', 'verloren', 'magisch'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Schutz'},
+			{adjectiveEnding: 'es', description: 'Versteck'},
+			{adjectiveEnding: 'e', description: 'Hütte'},
+			{adjectiveEnding: 'es', description: 'Häuschen'},
+		]
 	},
 	{
 		// 3 * 3
@@ -187,8 +341,12 @@ export const combinations: ICombination[] = [
 		total: 8706,
 		biggestCluster: 1008,
 		clusters: 326,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'fein', 'angenehm'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Rastbank'},
+			{adjectiveEnding: 'er', description: 'Sitzplatz'},
+			{adjectiveEnding: 'er', description: 'Platz'},
+		]
 	},
 	{
 		// 3 * 3
@@ -196,8 +354,12 @@ export const combinations: ICombination[] = [
 		total: 7783,
 		biggestCluster: 1008,
 		clusters: 148,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'schützend', 'sicher'],
+		descriptions: [
+			{adjectiveEnding: 'es', description: 'Versteck' },
+			{adjectiveEnding: 'er', description: 'Hochsitz'},
+			{adjectiveEnding: 'er', description: 'Hochstand'},
+		]
 	},
 	{
 		// 3 * 3
@@ -205,8 +367,12 @@ export const combinations: ICombination[] = [
 		total: 6908,
 		biggestCluster: 986,
 		clusters: 251,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'historisch', 'honorig'],
+		descriptions: [
+			{adjectiveEnding: 'es', description: 'Denkmal'},
+			{adjectiveEnding: 'e', description: 'Gedenkstätte'},
+			{adjectiveEnding: 'es', description: 'Monument'},
+		]
 	},
 	{
 		// 3 * 3
@@ -214,8 +380,12 @@ export const combinations: ICombination[] = [
 		total: 5027,
 		biggestCluster: 904,
 		clusters: 219,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'belebt', 'lebhaft'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Bahnhof'},
+			{adjectiveEnding: 'e', description: 'Haltestelle'},
+			{adjectiveEnding: 'e', description: 'Station'},
+		]
 	},
 	{
 		// 3 * 3
@@ -223,8 +393,12 @@ export const combinations: ICombination[] = [
 		total: 5470,
 		biggestCluster: 863,
 		clusters: 187,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'gewaltig', 'mächtig'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Turmuhr'},
+			{adjectiveEnding: 'e', description: 'Uhr'},
+			{adjectiveEnding: 'es', description: 'Zeitmessgerät'},
+		]
 	},
 	{
 		// 3 * 3
@@ -232,8 +406,12 @@ export const combinations: ICombination[] = [
 		total: 5019,
 		biggestCluster: 851,
 		clusters: 174,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'luftig', 'edl'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Hochstand'},
+			{adjectiveEnding: 'er', description: 'Schutz'},
+			{adjectiveEnding: 'es', description: 'Versteck'},
+		]
 	},
 	{
 		// 3 * 3
@@ -241,8 +419,12 @@ export const combinations: ICombination[] = [
 		total: 5721,
 		biggestCluster: 835,
 		clusters: 174,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'mystisch', 'schattig'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Stand'},
+			{adjectiveEnding: 'er', description: 'Unterschlupf'},
+			{adjectiveEnding: 'er', description: 'Bunker'},
+		]
 	},
 	{
 		// 3 * 3
@@ -250,8 +432,12 @@ export const combinations: ICombination[] = [
 		total: 7175,
 		biggestCluster: 821,
 		clusters: 259,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'behaglich', 'fein'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Bank'},
+			{adjectiveEnding: 'er', description: 'Schemel'},
+			{adjectiveEnding: 'e', description: 'Rastgelegenheit'}
+		]
 	},
 	{
 		// 3 * 3
@@ -259,8 +445,12 @@ export const combinations: ICombination[] = [
 		total: 3408,
 		biggestCluster: 694,
 		clusters: 172,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'charmant', 'lokal'],
+		descriptions: [
+			{adjectiveEnding: 'es', description: 'Posthaus'},
+			{adjectiveEnding: 'es', description: 'Postamt'},
+			{adjectiveEnding: 'es', description: 'Postgebäude'},
+		]
 	},
 	{
 		// 3 * 3
@@ -268,8 +458,12 @@ export const combinations: ICombination[] = [
 		total: 1989,
 		biggestCluster: 627,
 		clusters: 162,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'ulkig', 'einladend'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Gasthof'},
+			{adjectiveEnding: 'e', description: 'Gaststätte'},
+			{adjectiveEnding: 'es', description: 'Wirtshaus'},
+		]
 	},
 	{
 		// 3 * 3
@@ -277,8 +471,12 @@ export const combinations: ICombination[] = [
 		total: 3667,
 		biggestCluster: 603,
 		clusters: 212,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'bedeutungsvoll', 'altertümlich'],
+		descriptions: [
+			{adjectiveEnding: 'e', description: 'Denkstätte'},
+			{adjectiveEnding: 'er', description: 'Schauplatz'},
+			{adjectiveEnding: 'er', description: 'Ort'},
+		]
 	},
 	{
 		// 3 * 3
@@ -286,8 +484,12 @@ export const combinations: ICombination[] = [
 		total: 1912,
 		biggestCluster: 580,
 		clusters: 150,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'fröhlich', 'erheiternd'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Festplatz'},
+			{adjectiveEnding: 'e', description: 'Spielstätte'},
+			{adjectiveEnding: 'e', description: 'Raststätte'},
+		]
 	},
 	{
 		// 3 * 3
@@ -295,8 +497,12 @@ export const combinations: ICombination[] = [
 		total: 2102,
 		biggestCluster: 568,
 		clusters: 123,
-		adjectives: [],
-		descriptions: []
+		adjectives: ['', 'mystisch', 'kurios'],
+		descriptions: [
+			{adjectiveEnding: 'er', description: 'Ort'},
+			{adjectiveEnding: 'er', description: 'Platz'},
+			{adjectiveEnding: 'e', description: 'Gegend'},
+		]
 	},
 	{
 		// 3 * 3
@@ -304,7 +510,7 @@ export const combinations: ICombination[] = [
 		total: 3198,
 		biggestCluster: 480,
 		clusters: 221,
-		adjectives: ['', 'einladend'],
+		adjectives: ['', 'einladend', 'annehmbar'],
 		descriptions: [
 			{adjectiveEnding: 'er', description: 'Picknick-Tisch'},
 			{adjectiveEnding: 'er', description: 'Brotzeitplatz'},
